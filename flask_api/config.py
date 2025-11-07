4286a1e9f498eafd24672fc1b404ea0fc563a352
Gustavo Vechiatto <147154648+Gustavovechiatto@users.noreply.github.com>
2025-11-07 10:02:36 -0300

    Delete flask_api directory

--- flask_api/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///escola.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "sua_chave_secreta_aqui"
--- flask_api/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .professor import Professor
-from .turma import Turma
-from .aluno import Aluno
